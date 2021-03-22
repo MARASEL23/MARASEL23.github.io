a872ae3d4bce65911bb7493d1b0f5cfada295607
MARASEL23 <yusufoz230644@gmail.com>
2021-03-22 15:48:18 +0300

    PUSH

---
+++ Scripts/Pervane.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pervane : MonoBehaviour
+{
+    public float hız = 1000f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,0,hız);
+        
+    } değişirsem yol değiştiği için hata verirler mi
+}
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //private variables
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //player inputs
+      horizontalInput = Input.GetAxis("Horizontal");
+      forwardInput = Input.GetAxis("Vertical");
+
+       // we move the vehicle
+       transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+       //turn the vehicle
+       transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
